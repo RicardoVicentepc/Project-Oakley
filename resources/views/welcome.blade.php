725a95a2390d67320724f50bfb1b348e90041953
Ricardo Vicente <86174349+RicardoVicentepc@users.noreply.github.com>
2022-10-31 17:38:29 -0300

    ColocandoPDF
    Coloquei o pdf só falta estilar a folha com os id, names e values separados para ter dados exatos.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,11 +12,7 @@
 
 
 <main>
-    <form class="search-container" action="/ass">
-        <h1 class="titleHome">Busque pela a categoria</h1>
-        <input type="text" id="search-bar" placeholder="Buscar">
-        <a href="#"><img class="search-icon" src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"></a>
-    </form>
+ 
     
 </main>
 
